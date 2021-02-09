f4de40c45374be7a7d3477f64becbe3e3362e1ec
Rebecca Samuel <se.rebecca.samuel@gmail.com>
2021-02-09 09:45:13 +0300

    Started Working on task manager

---
+++ TaskManager/assets/js/storage.js
@@ -0,0 +1,26 @@
+// Add to LocalStorage function declaration 
+function addToDatabase(newTask) {
+    let listofTasks;
+    if (localStorage.getItem('tasks') == null) {
+        listofTasks = [];
+    } 
+    else {
+        listofTasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    listofTasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(listofTasks));
+}
+
+//Load task from local storage function declaration
+function loadfromDB() {
+    let listofTasks;
+
+    if (localStorage.getItem('tasks') == null) {
+        listofTasks = [];
+    }
+    else {
+        listofTasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return listofTasks; //return array
+}
